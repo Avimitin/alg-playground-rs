50aadf14438ec23d8d44e2b005ce8fbcf3000be0
Avimitin <avimitin@gmail.com>
2022-03-04 12:34:38 +0800

    utils: add script to generate leetcode template
    Signed-off-by: Avimitin <avimitin@gmail.com>

---
+++ new.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -ex
+
+if [[ -z "$1" ]]; then
+  echo "No argument"
+  exit 1
+fi
+
+FILE_NAME=${1//-/_}
+TEMPLATE="#[allow(dead_code)]
+struct Solution;
+
+impl Solution {
+    #[allow(dead_code)]
+
+}
+
+#[test]
+fn test() {
+
+}"
+
+echo "$TEMPLATE" > ./src/leetcode_cn/$FILE_NAME.rs
+echo "mod $FILE_NAME;" >> ./src/leetcode_cn/mod.rs
