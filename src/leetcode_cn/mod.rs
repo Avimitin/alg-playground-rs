3f5654dba6fa5a4fe7f126a4a2b814f7ff8d0896
Avimitin <avimitin@gmail.com>
2022-03-04 21:50:02 +0800

    leetcode: sort the module
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/leetcode_cn/mod.rs
+++ src/leetcode_cn/mod.rs
@@ -1,14 +1,5 @@
-// 48
-mod rotate_image;
-
-// 392
-mod is_subsequence;
-
-// 468
-mod validate_ip_address;
-
-// 1525
-mod number_of_good_ways_to_split_a_string;
-
-// 1736
-mod latest_time_by_replacing_hidden_digits;
+mod rotate_image; // 48
+mod is_subsequence; // 392
+mod validate_ip_address; // 468
+mod number_of_good_ways_to_split_a_string; // 1525
+mod latest_time_by_replacing_hidden_digits; // 1736
