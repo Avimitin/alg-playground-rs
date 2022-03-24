6e3baa06bff5b80fe2001f5064a3a60b83188d52
Avimitin <avimitin@gmail.com>
2022-03-24 22:20:39 +0800

    leetcode: improve constant complexity
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/leetcode_cn/boats_to_save_people.rs
+++ src/leetcode_cn/boats_to_save_people.rs
@@ -4,12 +4,11 @@ struct Solution;
 impl Solution {
     #[allow(dead_code)]
     #[cold]
-    pub fn num_rescue_boats(people: Vec<i32>, limit: i32) -> i32 {
+    pub fn num_rescue_boats(mut people: Vec<i32>, limit: i32) -> i32 {
         if people.is_empty() {
             return 0;
         }
 
-        let mut people = people.clone();
         people.sort_unstable();
 
         let (mut i, mut j, mut ans) = (people.len() - 1, 0, 0);
