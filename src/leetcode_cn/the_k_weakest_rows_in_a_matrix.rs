b4ad747d1487559232535d0107349656c9b6e25a
Avimitin <avimitin@gmail.com>
2022-03-30 09:02:53 +0800

    leetcode: let rust guess the vec type
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/leetcode_cn/the_k_weakest_rows_in_a_matrix.rs
+++ src/leetcode_cn/the_k_weakest_rows_in_a_matrix.rs
@@ -4,7 +4,7 @@ struct Solution;
 impl Solution {
     #[allow(dead_code)]
     pub fn k_weakest_rows(mat: Vec<Vec<i32>>, k: i32) -> Vec<i32> {
-        let mut rec = Vec::from(Vec::new());
+        let mut rec = Vec::new();
 
         for (i, row) in mat.iter().enumerate() {
             let sum: i32 = row.iter().sum();
