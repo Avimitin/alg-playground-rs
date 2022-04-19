c6601a97e97173dca41fa23cddfbfc559cb73823
Avimitin <avimitin@gmail.com>
2022-04-19 09:54:16 +0800

    lc: improve 99
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/leetcode_cn/recover_binary_search_tree.rs
+++ src/leetcode_cn/recover_binary_search_tree.rs
@@ -11,42 +11,46 @@ use std::rc::Rc;
 #[allow(dead_code)]
 struct Solution;
 
+#[derive(Default)]
+struct Runtime {
+    prev: Option<Rc<RefCell<TreeNode>>>,
+    // swap below
+    first: Option<Rc<RefCell<TreeNode>>>,
+    second: Option<Rc<RefCell<TreeNode>>>,
+}
+
 impl Solution {
     #[allow(dead_code)]
     pub fn recover_tree(root: &mut Option<Rc<RefCell<TreeNode>>>) {
-        let mut vec = Vec::new();
-        Self::traverse(root.clone(), &mut vec);
-        if vec.is_empty() {
-            return;
-        }
-        vec.sort_unstable();
+        let mut rt = Runtime::default();
+        Self::traverse(root.clone(), &mut rt);
 
-        Self::updtree(root, &vec);
+        let first = rt.first.unwrap();
+        let second = rt.second.unwrap();
+
+        let tmp = first.borrow().val;
+        first.borrow_mut().val = second.borrow().val;
+        second.borrow_mut().val = tmp;
     }
 
-    fn traverse(root: Option<Rc<RefCell<TreeNode>>>, s: &mut Vec<i32>) -> Option<()> {
+    fn traverse(root: Option<Rc<RefCell<TreeNode>>>, rt: &mut Runtime) -> Option<()> {
         let node = root?;
-        Self::traverse(node.borrow().left.clone(), s);
-        s.push(node.borrow().val);
-        Self::traverse(node.borrow().right.clone(), s);
+        Self::traverse(node.borrow().left.clone(), rt);
+        let val = node.borrow().val;
+        if let Some(prev_node) = &rt.prev {
+            if val < prev_node.borrow().val {
+                if rt.first.is_none() {
+                    rt.first = Some(prev_node.clone());
+                }
+
+                rt.second = Some(node.clone());
+            }
+        }
+
+        rt.prev = Some(node.clone());
+        Self::traverse(node.borrow().right.clone(), rt);
 
         Some(())
-    }
-
-    fn updtree(root: &mut Option<Rc<RefCell<TreeNode>>>, v: &[i32]) {
-        let mut i = 0;
-        Self::fixtree(root, v, &mut i);
-    }
-
-    fn fixtree(root: &mut Option<Rc<RefCell<TreeNode>>>, v: &[i32], i: &mut usize) {
-        if root.is_none() {
-            return;
-        }
-        let node = root.as_mut().unwrap();
-        Self::fixtree(&mut node.borrow_mut().left.clone(), v, i);
-        node.borrow_mut().val = v[*i];
-        *i += 1;
-        Self::fixtree(&mut node.borrow_mut().right.clone(), v, i);
     }
 }
 
