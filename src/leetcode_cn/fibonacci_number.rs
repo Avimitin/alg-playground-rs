227395fcb192197bafc80a99c89a61aeac1e98e4
Avimitin <avimitin@gmail.com>
2022-07-06 10:22:25 +0800

    lc: improve 509
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/leetcode_cn/fibonacci_number.rs
+++ src/leetcode_cn/fibonacci_number.rs
@@ -2,15 +2,19 @@ struct Solution;
 
 impl Solution {
     pub fn fib(n: i32) -> i32 {
-        if n == 0 {
-            return 0;
+        if n <= 1 {
+            return n;
         }
 
-        if n == 1 {
-            return 1;
+        let (mut a, mut b) = (0, 1);
+
+        for _ in 2..=n {
+            let sum = a + b;
+            a = b;
+            b = sum;
         }
 
-        Self::fib(n - 1) + Self::fib(n - 2)
+        b
     }
 }
 
